cbbfbaf4a3e6eb46f92a4126e94582d22c5880b7
PDOBA <128619658+PDOBA@users.noreply.github.com>
2024-11-28 08:41:50 +0900

    minify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true, // Helps catch potential issues in React
+    swcMinify: true,       // Enable SWC minification for faster builds
 };
 
 export default nextConfig;
