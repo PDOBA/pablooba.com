80e9a673c3efd641523f8dc8655cd8a3cfc853f9
PDOBA <pablo.oba@outlook.com>
2024-11-28 20:03:01 +0900

    minify gone

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: true, // Helps catch potential issues in React
-    swcMinify: false,       // Enable SWC minification for faster builds
 };
 
 export default nextConfig;
